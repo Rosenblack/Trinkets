eabbcdbf96d20dcb8adeff89bc4a208375df3c2a
Lucy <gakupo.gumi@gmail.com>
2023-02-18 09:40:18 +1300

    Int Release

---
+++ src/main/java/com/shale/horde/trinkets/Client.java
@@ -0,0 +1,10 @@
+package com.shale.horde.trinkets;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
